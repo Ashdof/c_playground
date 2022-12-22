e9022f276045eda3bce22e41cc20b926b096d116
Ashdof <mzigah4@gmail.com>
2022-12-22 15:25:34 +0000

    defined two structs for hash table and hash table items

--- lessons/datastructures/hashtables/lists.h
+++ lessons/datastructures/hashtables/lists.h
@@ -4,6 +4,36 @@
 #include <stdio.h>
 #include "hashfunctions.c"
 
+
+/**
+ * Ht_item - define an item for the hash table
+ *
+ * Description: this struct defines the elements for the hash
+ * table items
+ */
+
+typedef struct Ht_item
+{
+	char *key;
+	char *value;
+}ht_item;
+
+/**
+ * HashTable - define a hash table
+ *
+ * Description: this struct defines hash table
+ */
+
+typedef struct HashTable
+{
+	ht_item **items;	/* a reference pointer to the items */
+	int size;		/* size of the hash table */
+	int count;		/* number of elements in the hash table */
+}hashTable;
+
+
+/* list of prototypes */
+
 unsigned long int hash_func(char *_str);
 
 #endif /* LISTS_H */ 
